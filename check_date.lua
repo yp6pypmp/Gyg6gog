08209efb0e5b046291c908126a1136bc326b4ddb
yp6pypmp <pyp3py@gmail.com>
2025-07-11 20:24:22 +0900

    check_date.lua

--- check_date.lua
+++ check_date.lua
@@ -1,11 +1,9 @@
--- check_date.lua（GitHub側）
-local limit = {year = 2024, month = 7, day = 10} -- ← 期限を過去にしてテスト用
+local limit = {year = 2025, month = 7, day = 10}
 local now = os.date("*t")
 
 if now.year > limit.year or
    (now.year == limit.year and now.month > limit.month) or
    (now.year == limit.year and now.month == limit.month and now.day > limit.day) then
-  gg.alert("❌ 有効期限切れ：このスクリプトは使用できません。")
-  os.exit()
-  return 
+  gg.alert("❌ 有効期限切れ。このスクリプトは使用できません。")
+  error("expired") 
 end
