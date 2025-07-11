811e58cab5d780b75a08b01fbaf2ff383a60e3d4
yp6pypmp <pyp3py@gmail.com>
2025-07-11 20:44:37 +0900

    check_date.lua

--- check_date.lua
+++ check_date.lua
@@ -1,8 +1,1 @@
-local limit = {year = 2025, month = 7, day = 10}
-local now = os.date("*t")
-
-if now.year > limit.year or
-   (now.year == limit.year and now.month > limit.month) or
-   (now.year == limit.year and now.month == limit.month and now.day > limit.day) then
-  error("expired") 
-end
+2025-07-15
